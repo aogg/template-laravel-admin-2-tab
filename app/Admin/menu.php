f0af9b3fcd7264921474ffd467e84c041bb3cb64
aogg <8998031+aogg@users.noreply.github.com>
2024-11-01 15:05:28 +0800

    common
    代码写死菜单

---
+++ app/Admin/menu.php
@@ -0,0 +1,22 @@
+<?php
+
+
+return [
+
+    [
+        'id'        => 'tables',
+        'title'     => '列表',
+        'icon'      => 'feather icon-grid',
+        'uri'       => '',
+        'parent_id' => 0,
+    ],
+
+    [
+        'id'        => 'grid',
+        'title'     => '默认表格',
+        'icon'      => 'feather icon-menu',
+        'uri'       => 'components/grid',
+        'parent_id' => 'tables',
+    ],
+
+];
