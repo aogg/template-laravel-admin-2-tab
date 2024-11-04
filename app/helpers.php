2837bf8a08effe0acb15bd9b41dae1f6e253c2fc
aozhuochao <aozhuochao@aozhuochao.com>
2024-11-04 16:16:39 +0800

    common   trans_arr_global
    trans_arr_global

--- app/helpers.php
+++ app/helpers.php
@@ -2,3 +2,14 @@
 
 // 系统方法
 include_once __DIR__ . '/helpers_common.php';
+
+
+
+function trans_arr_global(...$arr){
+    $newArr = [];
+    foreach ($arr as $item) {
+        $newArr[] = 'global.' . $item;
+    }
+
+    return trans_arr(...$newArr);
+}
