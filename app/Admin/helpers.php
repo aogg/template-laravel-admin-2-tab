679f46d47dda55984d3d5ca193c9a154aeab3b72
aogg <8998031+aogg@users.noreply.github.com>
2024-11-04 19:56:45 +0800

    common   helper_common.php
    helper_common.php
    
    (cherry picked from commit 31c8798c34e34fee84f9acc32f1d65b435cadd95)

--- app/Admin/helpers.php
+++ app/Admin/helpers.php
@@ -1,36 +1,3 @@
 <?php
 
-use Illuminate\Support\Arr;
-
-if (! function_exists('user_admin_config')) {
-    function user_admin_config($key = null, $value = null)
-    {
-        $cache = cache();
-
-        $cacheKey = 'admin:config:' . \Dcat\Admin\Admin::user()?->id;
-        if (! $config = $cache->get($cacheKey)) {
-            $config = [];
-            $config['layout'] = config('admin.layout');
-            $config['helpers'] = config('admin.helpers');
-
-            $config['lang'] = config('app.locale');
-        }
-
-        if (is_array($key)) {
-            // 保存
-            foreach ($key as $k => $v) {
-                Arr::set($config, $k, $v);
-            }
-
-            $cache->set($cacheKey, $config);
-
-            return;
-        }
-
-        if ($key === null) {
-            return $config;
-        }
-
-        return Arr::get($config, $key, $value);
-    }
-}
+include_once __DIR__ . '/helper_common.php';
