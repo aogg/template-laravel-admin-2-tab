4c514a07df00988a17b17f005aa20570edfe15c4
aozhuochao <aozhuochao@aozhuochao.com>
2024-11-12 18:10:19 +0800

    common    GridTool.php

--- app/Admin/helper_common.php
+++ app/Admin/helper_common.php
@@ -4,53 +4,6 @@
 use Illuminate\Support\Arr;
 
 /**
- * 自动点击刷新按钮
- *
- * @return void
- */
-function admin_grid_auto_refresh($seconds = 10){
-    \Dcat\Admin\Admin::script(
-        <<<JS
-(function (){
-    let uncheckBool = false;
-    setTimeout(function (){
-        if (uncheckBool){
-            return;
-        }
-
-        let localTime = window['admin_grid_auto_refresh_time'];
-
-        if(localTime && (localTime + 10 * 1000) > Date.now()) {
-            return;
-         }
-        window['admin_grid_auto_refresh_time'] = Date.now();
-
-        console.log('自动刷新--' + localTime)
-        $('button.grid-refresh').click();
-    }, $seconds * 1000);
-
-function checkVisibility() {
-    if (document.hidden) {
-        console.log("当前窗口不可见");
-        uncheckBool = true;
-    } else {
-        console.log("当前窗口可见");
-        uncheckBool = false;
-    }
-}
-
-// 监听可见性变化事件
-document.addEventListener("visibilitychange", checkVisibility);
-
-// 初始检查
-checkVisibility();
-})()
-JS
-
-    );
-}
-
-/**
  * 处理attribute写法复杂问题
  *
  * @param $gridOrShow
@@ -68,22 +21,6 @@ function admin_handle_attribute($gridOrShow, $fieldOrigin, $showField){
             return $this->{$showField};
         });
     }
-}
-
-/**
- * 列表--整形在线编辑
- *
- * @param \Dcat\Admin\Grid $grid
- * @param ...$columnArr
- * @return void
- */
-function admin_handle_grid_editable_integer($grid, ...$columnArr){
-
-    $grid->column('max_create_num')->editable([
-        'mask' => [
-            'alias' => 'integer',
-        ],
-    ]);
 }
 
 /**
