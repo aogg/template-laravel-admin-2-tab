549cccf3d725e8113c411872fc084269067774ee
aozhuochao <aozhuochao@aozhuochao.com>
2024-11-14 12:11:38 +0800

    /* 写死的菜单 无法依靠界面操作 */

--- app/Admin/menu.php
+++ app/Admin/menu.php
@@ -1,22 +1,23 @@
 <?php
 
+/* 写死的菜单 无法依靠界面操作 */
 
 return [
 
-    [
-        'id'        => 'tables',
-        'title'     => '列表',
-        'icon'      => 'feather icon-grid',
-        'uri'       => '',
-        'parent_id' => 0,
-    ],
-
-    [
-        'id'        => 'grid',
-        'title'     => '默认表格',
-        'icon'      => 'feather icon-menu',
-        'uri'       => 'components/grid',
-        'parent_id' => 'tables',
-    ],
+//    [
+//        'id'        => 'tables',
+//        'title'     => '列表',
+//        'icon'      => 'feather icon-grid',
+//        'uri'       => '',
+//        'parent_id' => 0,
+//    ],
+//
+//    [
+//        'id'        => 'grid',
+//        'title'     => '默认表格',
+//        'icon'      => 'feather icon-menu',
+//        'uri'       => 'components/grid',
+//        'parent_id' => 'tables',
+//    ],
 
 ];
