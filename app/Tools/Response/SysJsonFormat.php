97e7ee4a675be9bab8f6ac06cf78f115891cbfd9
aogg <8998031+aogg@users.noreply.github.com>
2024-11-15 17:15:30 +0800

    common SysJsonFormat throw

--- app/Tools/Response/SysJsonFormat.php
+++ app/Tools/Response/SysJsonFormat.php
@@ -15,5 +15,15 @@ class SysJsonFormat extends JsonFormat
         return trans_arr_global('success');
     }
 
+    public static function throw($msg)
+    {
+        if (defined('IS_ADMIN') && IS_ADMIN) {
+            throw new \Illuminate\Http\Exceptions\HttpResponseException(\Dcat\Admin\Admin::json()->error($msg)->send())
+            ;
+        }else{
+            throw new \Illuminate\Http\Exceptions\HttpResponseException(response()->json(static::errorMsg($msg)));
+        }
+
+    }
 
 }
